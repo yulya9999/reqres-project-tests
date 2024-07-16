3f2fa629e0d171d928a8ed36383c30424e699b85
Yulia Vdovina <yulya_vdovina_2000@mail.ru>
2024-07-16 11:34:10 +0300

    HW17

---
+++ schemas/user.py
@@ -0,0 +1,271 @@
+users_list = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "page": {
+      "type": "integer"
+    },
+    "per_page": {
+      "type": "integer"
+    },
+    "total": {
+      "type": "integer"
+    },
+    "total_pages": {
+      "type": "integer"
+    },
+    "data": {
+      "type": "array",
+      "items": [
+        {
+          "type": "object",
+          "properties": {
+            "id": {
+              "type": "integer"
+            },
+            "email": {
+              "type": "string"
+            },
+            "first_name": {
+              "type": "string"
+            },
+            "last_name": {
+              "type": "string"
+            },
+            "avatar": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "avatar"
+          ]
+        },
+        {
+          "type": "object",
+          "properties": {
+            "id": {
+              "type": "integer"
+            },
+            "email": {
+              "type": "string"
+            },
+            "first_name": {
+              "type": "string"
+            },
+            "last_name": {
+              "type": "string"
+            },
+            "avatar": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "avatar"
+          ]
+        },
+        {
+          "type": "object",
+          "properties": {
+            "id": {
+              "type": "integer"
+            },
+            "email": {
+              "type": "string"
+            },
+            "first_name": {
+              "type": "string"
+            },
+            "last_name": {
+              "type": "string"
+            },
+            "avatar": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "avatar"
+          ]
+        },
+        {
+          "type": "object",
+          "properties": {
+            "id": {
+              "type": "integer"
+            },
+            "email": {
+              "type": "string"
+            },
+            "first_name": {
+              "type": "string"
+            },
+            "last_name": {
+              "type": "string"
+            },
+            "avatar": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "avatar"
+          ]
+        },
+        {
+          "type": "object",
+          "properties": {
+            "id": {
+              "type": "integer"
+            },
+            "email": {
+              "type": "string"
+            },
+            "first_name": {
+              "type": "string"
+            },
+            "last_name": {
+              "type": "string"
+            },
+            "avatar": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "avatar"
+          ]
+        },
+        {
+          "type": "object",
+          "properties": {
+            "id": {
+              "type": "integer"
+            },
+            "email": {
+              "type": "string"
+            },
+            "first_name": {
+              "type": "string"
+            },
+            "last_name": {
+              "type": "string"
+            },
+            "avatar": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "avatar"
+          ]
+        }
+      ]
+    },
+    "support": {
+      "type": "object",
+      "properties": {
+        "url": {
+          "type": "string"
+        },
+        "text": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "url",
+        "text"
+      ]
+    }
+  },
+  "required": [
+    "page",
+    "per_page",
+    "total",
+    "total_pages",
+    "data",
+    "support"
+  ]
+}
+
+create_users = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "name": {
+      "type": "string"
+    },
+    "job": {
+      "type": "string"
+    },
+    "id": {
+      "type": "string"
+    },
+    "createdAt": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "name",
+    "job",
+    "id",
+    "createdAt"
+  ]
+}
+
+update_user_info = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "name": {
+      "type": "string"
+    },
+    "job": {
+      "type": "string"
+    },
+    "updatedAt": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "name",
+    "job",
+    "updatedAt"
+  ]
+}
+
+register_user = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "id": {
+      "type": "integer"
+    },
+    "token": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "id",
+    "token"
+  ]
+}
---
+++ tests/test_users.py
@@ -0,0 +1,85 @@
+import requests
+from jsonschema import validate
+from schemas.user import *
+
+url = 'https://reqres.in/api'
+
+
+def test_all_the_users_should_have_unique_id():
+    response = requests.get(url=f'{url}/users')
+    body = response.json()
+    ids = [element['id'] for element in body['data']]
+
+    assert response.status_code == 200
+    assert body['per_page'] == 6
+    assert list(set(ids)) == ids
+    validate(body, schema=users_list)
+
+
+def test_get_user_id_does_not_exist():
+    id_user = '23'
+    response = requests.get(url=f'{url}/users/{id_user}')
+    body = response.json()
+
+    assert response.status_code == 404
+    assert body == {}
+
+
+def test_post_create_user():
+    payload = {
+        "name": "Yulia",
+        "job": "tester"
+    }
+    response = requests.post(url=f'{url}/users', data=payload)
+    body = response.json()
+
+    assert response.status_code == 201
+    assert body['name'] == "Yulia"
+    assert body['job'] == "tester"
+    validate(body, schema=create_users)
+    return body['id']
+
+
+def test_post_register_user_successful():
+    payload = {
+        "email": "eve.holt@reqres.in",
+        "password": "pistol"
+    }
+    response = requests.post(url=f'{url}/register', data=payload)
+    body = response.json()
+
+    assert response.status_code == 200
+    validate(body, schema=register_user)
+
+
+def test_post_register_user_unsuccessful():
+    payload = {
+        "email": "yul@reqres.in"
+    }
+
+    response = requests.post(url=f'{url}/register', data=payload)
+    body = response.json()
+
+    assert response.status_code == 400
+    assert body['error'] == 'Missing password'
+
+
+def test_put_update_user_info():
+    payload = {
+        "name": "Yulia",
+        "job": "test engineer"
+    }
+
+    response = requests.put(url=f'{url}/users/{test_post_create_user()}', data=payload)
+    body = response.json()
+
+    assert response.status_code == 200
+    assert body['job'] == "test engineer"
+    validate(body, schema=update_user_info)
+
+
+def test_delete_user_by_id():
+    response = requests.delete(url=f'{url}/users/{test_post_create_user()}')
+
+    assert response.status_code == 204
+    assert response.text == ""
