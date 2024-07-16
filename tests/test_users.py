50abeabc2f3de78575ec82d44e67aae075bbf4ca
Yulia Vdovina <yulya_vdovina_2000@mail.ru>
2024-07-16 13:47:56 +0300

    HW17

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,12 +1,10 @@
 import requests
 from jsonschema import validate
-from schemas.user import *
-
-url = 'https://reqres.in/api'
+from schemas.user import users_list, create_users, register_user, update_user_info
 
 
-def test_all_the_users_should_have_unique_id():
-    response = requests.get(url=f'{url}/users')
+def test_all_the_users_should_have_unique_id(base_url):
+    response = requests.get(url=f'{base_url}/users')
     body = response.json()
     ids = [element['id'] for element in body['data']]
 
@@ -16,36 +14,35 @@ def test_all_the_users_should_have_unique_id():
     validate(body, schema=users_list)
 
 
-def test_get_user_id_does_not_exist():
+def test_get_user_id_does_not_exist(base_url):
     id_user = '23'
-    response = requests.get(url=f'{url}/users/{id_user}')
+    response = requests.get(url=f'{base_url}/users/{id_user}')
     body = response.json()
 
     assert response.status_code == 404
     assert body == {}
 
 
-def test_post_create_user():
+def test_post_create_user(base_url):
     payload = {
         "name": "Yulia",
         "job": "tester"
     }
-    response = requests.post(url=f'{url}/users', data=payload)
+    response = requests.post(url=f'{base_url}/users', data=payload)
     body = response.json()
 
     assert response.status_code == 201
     assert body['name'] == "Yulia"
     assert body['job'] == "tester"
     validate(body, schema=create_users)
-    return body['id']
 
 
-def test_post_register_user_successful():
+def test_post_register_user_successful(base_url):
     payload = {
         "email": "eve.holt@reqres.in",
         "password": "pistol"
     }
-    response = requests.post(url=f'{url}/register', data=payload)
+    response = requests.post(url=f'{base_url}/register', data=payload)
     body = response.json()
 
     assert response.status_code == 200
@@ -54,25 +51,26 @@ def test_post_register_user_successful():
     validate(body, schema=register_user)
 
 
-def test_post_register_user_unsuccessful():
+def test_post_register_user_unsuccessful(base_url):
     payload = {
         "email": "yul@reqres.in"
     }
 
-    response = requests.post(url=f'{url}/register', data=payload)
+    response = requests.post(url=f'{base_url}/register', data=payload)
     body = response.json()
 
     assert response.status_code == 400
     assert body['error'] == 'Missing password'
 
 
-def test_put_update_user_info():
+def test_put_update_user_info(base_url):
     payload = {
         "name": "Yulia",
         "job": "test engineer"
     }
+    user_id = '2'
 
-    response = requests.put(url=f'{url}/users/{test_post_create_user()}', data=payload)
+    response = requests.put(url=f'{base_url}/users/{user_id}', data=payload)
     body = response.json()
 
     assert response.status_code == 200
@@ -80,8 +78,9 @@ def test_put_update_user_info():
     validate(body, schema=update_user_info)
 
 
-def test_delete_user_by_id():
-    response = requests.delete(url=f'{url}/users/{test_post_create_user()}')
+def test_delete_user_by_id(base_url):
+    user_id = '2'
+    response = requests.delete(url=f'{base_url}/users/{user_id}')
 
     assert response.status_code == 204
     assert response.text == ""
